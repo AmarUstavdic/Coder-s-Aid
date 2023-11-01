eed2f9f086c5054bf4011d5f925fc8c6d0b8e95a
lilwizzz <amar.ustavdija@gmail.com>
2023-11-01 13:58:25 +0100

    initialized react, refactored util.js

--- util/util.js
+++ util/util.js
@@ -1,6 +1,5 @@
 const cheerio = require('cheerio');
 
-
 const CHA = 'abcdefghijklmnopqrstuvwxyz0123456789'
 
 
@@ -19,59 +18,6 @@ function rndString(n) {
 function rndInt(n) {
     return Math.floor(Math.random() * n);
 }
-
-/** 
- * parese ftaa from inital get request for /enter
-*/
-function parseFtaa(data) {
-    const $ = cheerio.load(data)
-    const scriptTag = $('script:contains("window._ftaa")')
-
-    if (scriptTag.length > 0) {
-        const scriptContent = scriptTag.html();
-        const match = scriptContent.match(/window._ftaa = "(.*?)"/)
-
-        if (match) {
-            const value = match[1]
-            return value
-        } else {
-            return ''
-        }
-    } else {
-        return '' // Script tag containing window._ftaa not found
-    }
-}
-
-/** 
- * parse bfaa from initial get request
-*/
-function parseBfaa(data) {
-    const $ = cheerio.load(data)
-    const scriptTag = $('script:contains("window._bfaa")')
-
-    if (scriptTag.length > 0) {
-        const scriptContent = scriptTag.html()
-        const match = scriptContent.match(/window._bfaa = "(.*?)"/)
-
-        if (match) {
-            const value = match[1]
-            return value
-        } else {
-            return ''
-        }
-    } else {
-        return ''
-    }
-}
-
-
-function parseCsrfToken(data) {
-    const $ = cheerio.load(data);
-    const csrfTokenElement = $('span.csrf-token');
-    const csrfToken = csrfTokenElement.attr('data-csrf');
-    return csrfToken
-}   
-
 
 
 module.exports = { rndString, parseFtaa, parseBfaa, parseCsrfToken}
